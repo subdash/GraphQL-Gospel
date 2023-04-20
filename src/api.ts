5fb2a224fba40bc5a61ed05ebf9f49f4f7670c8d
subdash <dhrs.dev@gmail.com>
2023-04-20 10:31:48 -0500

    Add lint, prettier, etc.

--- src/api.ts
+++ src/api.ts
@@ -4,26 +4,27 @@ import { Gospel, GospelResponse } from './generated/graphql';
 const BASE_URL = 'https://api.esv.org';
 
 class GospelAPI extends RESTDataSource {
-    override baseURL = BASE_URL;
+  override baseURL = BASE_URL;
 
-    async getSingleVerse(book: Gospel,
-                         chapter: number,
-                         verse: number
-    ): Promise<GospelResponse> {
-        const passageEndpoint = '/v3/passage/text/';
-        const { query, canonical, passages } = await this.get<GospelResponse>(passageEndpoint, {
-            params: { q: `${book}+${chapter}:${verse}` }
-        });
-        return {
-            query,
-            canonical,
-            passages
-        };
-    }
+  async getSingleVerse(book: Gospel, chapter: number, verse: number): Promise<GospelResponse> {
+    const passageEndpoint = '/v3/passage/text/';
+    const { query, canonical, passages } = await this.get<GospelResponse>(passageEndpoint, {
+      params: { q: `${book}+${chapter}:${verse}` }
+    });
+    return {
+      query,
+      canonical,
+      passages
+    };
+  }
 
-    override willSendRequest(_path: string, request: AugmentedRequest) {
-        request.headers['authorization'] = process.env.API_TOKEN;
+  override willSendRequest(_path: string, request: AugmentedRequest) {
+    const { API_TOKEN: apiToken } = process.env;
+    if (!apiToken) {
+      throw new Error('ESV API token undefined in environment');
     }
+    request.headers.authorization = apiToken;
+  }
 }
 
 export default GospelAPI;
--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,47 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { readFileSync } from 'fs';
 import { Resolvers } from './generated/graphql';
 
-
 import dotenv from 'dotenv';
 
 import GospelAPI from './api';
 
+if (!process.env.NODE_PATH) {
+  throw new Error('Env var NODE_PATH not configured');
+}
 dotenv.config({ path: `${process.env.NODE_PATH}/src/.env` });
 
 export interface Context {
-    dataSources: {
-        gospelAPI: GospelAPI
-    }
+  dataSources: {
+    gospelAPI: GospelAPI;
+  };
 }
 
 const typeDefs = readFileSync('src/schema.graphql', { encoding: 'utf-8' });
+
 const resolvers: Resolvers = {
-    Query: {
-        gospel: async (_, { by: { book, chapter, verse } }, { dataSources }) => {
-            const { gospelAPI: api } = dataSources;
-            const { query, canonical, passages } = await api.getSingleVerse(book, chapter, verse);
-            return { query, canonical, passages };
-        }
+  Query: {
+    gospel: async (_, { by: { book, chapter, verse } }, { dataSources }) => {
+      const { gospelAPI: api } = dataSources;
+      const { query, canonical, passages } = await api.getSingleVerse(book, chapter, verse);
+      return { query, canonical, passages };
     }
+  }
 };
+
 const server = new ApolloServer<Context>({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers
 });
+
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req, res }) => {
-        const { cache } = server;
-        return {
-            dataSources: {
-                gospelAPI: new GospelAPI({ cache }),
-            }
-        }
-    }
+  listen: { port: 4000 },
+  context: async (/* { req, res } */) => {
+    const { cache } = server;
+    return {
+      dataSources: {
+        gospelAPI: new GospelAPI({ cache })
+      }
+    };
+  }
 });
 console.log('Server ready at: ', url);
